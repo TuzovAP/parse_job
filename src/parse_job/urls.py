823ccf4b9b84539aaf0c04027641b666068a4f63
Aleksandr <tuzovap@gmail.com>
2023-03-20 20:32:58 +0300

    create app_scrapping

--- src/parse_job/urls.py
+++ src/parse_job/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home
+from app_scraping.views import home_view_app_scrapping
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    # path('home/', home),
+    path('home/', home_view_app_scrapping),
 ]
--- src/parse_job/views.py
+++ src/parse_job/views.py
@@ -7,4 +7,4 @@ def home(request):
     data = datetime.datetime.now()
     name = 'Sasha'
     django_dict = {'data': data, 'name': name}
-    return render(request, 'home.html', context=django_dict)
+    return render(request, 'base.html', context=django_dict)
