f658d423a1d5faa62f88d8613c0e15fc279e6bdd
Aleksandr <tuzovap@gmail.com>
2023-04-18 21:36:14 +0300

    Добавил страницы на сайт для нового пользователя, пункт в меню: зарегистрироваться

--- src/accounts_app/urls.py
+++ src/accounts_app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from accounts_app.views import login_view, logout_view
+from accounts_app.views import login_view, logout_view, register_view
 
 urlpatterns = [
     path('login/', login_view, name='login'),  # name - наименование относительной ссылки
     path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'),
 ]
--- src/accounts_app/views.py
+++ src/accounts_app/views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from accounts_app.forms import UserLoginForm
+from accounts_app.forms import UserLoginForm, UserRegistrationForm
 
 
 def login_view(request):
@@ -18,3 +19,15 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+def register_view(request):
+    form = UserRegistrationForm(request.POST or None)
+    if form.is_valid():
+        new_user = form.save(commit=False)
+        new_user.set_password(form.cleaned_data['password'])
+        new_user.save()
+        messages.success(request, 'Пользователь добавлен в систему.')
+        return render(request, 'accounts_app/register_done.html',
+                      {'new_user': new_user})
+    return render(request, 'accounts_app/register.html', {'form': form})
